16ae44b5b74ecd7799a51c944e7c2b9e2408c83b
Sviatoslav Samoilenko <sviatoslavs@wizardsdev.com>
2021-07-14 08:09:03 +0300

    Add project files.

---
+++ WebApplication2/Controllers/WeatherForecastController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static List<WeatherForecast> weatherForecasts = new List<WeatherForecast>();
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public void PostSomething(WeatherForecast weatherForecast)
+        {
+            weatherForecasts.Add(weatherForecast);
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return weatherForecasts;
+        }
+    }
+}
