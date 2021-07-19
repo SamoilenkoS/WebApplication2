17757865f0da72821f5aff05328b95e578f53190
Sviatoslav Samoilenko <sviatoslavs@wizardsdev.com>
2021-07-19 14:41:39 +0300

    Added automapper profile

---
+++ BussinessLayer/WeatherForecastProfile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+using DataAccessLayer;
+
+namespace BussinessLayer
+{
+    public class WeatherForecastProfile : Profile
+    {
+        public WeatherForecastProfile()
+        {
+            CreateMap<WeatherForecast, WeatherForecastDTO>()
+                .ForMember(x => x.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(x => x.Date, opt => opt.MapFrom(src => src.Date))
+                .ForMember(x => x.Summary, opt => opt.MapFrom(src => src.Summary))
+                .ForMember(x => x.TemperatureC, opt => opt.MapFrom(src => src.TemperatureC));
+
+            CreateMap<WeatherForecastDTO, WeatherForecast>()
+                .ForMember(x => x.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(x => x.Date, opt => opt.MapFrom(src => src.Date))
+                .ForMember(x => x.Summary, opt => opt.MapFrom(src => src.Summary))
+                .ForMember(x => x.TemperatureC, opt => opt.MapFrom(src => src.TemperatureC))
+                .ForMember(x => x.TemperatureF, opt => opt.MapFrom(src => src.TemperatureC * 2));
+        }
+    }
+}
